489ad43762cd2de7b127c39308f5560171b50d09
Argyn Khuan <argynkhuan@yandex.kz>
2020-04-04 18:11:06 +0600

    refactoring

--- src/main/java/io/hrmodule/hrservice/rest/EmployeeController.java
+++ src/main/java/io/hrmodule/hrservice/rest/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
