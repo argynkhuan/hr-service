df99076bcb8077b25448cc39a5a69bc9b1f72ee7
Argyn Khuan <argynkhuan@yandex.kz>
2020-05-08 18:05:04 +0600

    Candidate document refactoring

--- src/main/java/io/hrmodule/hrservice/document/Candidate.java
+++ src/main/java/io/hrmodule/hrservice/document/Candidate.java
@@ -29,5 +29,45 @@ public class Candidate {
     @NotNull
     private String email;
 
-    private String file;
+    private String attachment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
 }
