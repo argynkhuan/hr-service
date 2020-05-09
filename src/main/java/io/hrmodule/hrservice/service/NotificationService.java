bbb05febdde7ed209160404fcfa821a869ec9a60
Argyn Khuan <argynkhuan@yandex.kz>
2020-05-09 18:51:49 +0600

    Email service configuration

---
+++ src/main/java/io/hrmodule/hrservice/service/NotificationService.java
@@ -0,0 +1,30 @@
+package io.hrmodule.hrservice.service;
+
+import io.hrmodule.hrservice.document.Employee;
+import io.hrmodule.hrservice.service.email.EmailConfig;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class NotificationService {
+
+    private EmailConfig emailConfig;
+
+    public void sendMessage(Employee employee) {
+        // Create a mail sender
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(this.emailConfig.getHost());
+        mailSender.setPort(this.emailConfig.getPort());
+        mailSender.setUsername(this.emailConfig.getUsername());
+        mailSender.setPassword(this.emailConfig.getPassword());
+
+        // Create an email instance
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(employee.getEmail());
+        mailMessage.setTo("argynkhuan@yandex.kz");
+        mailMessage.setSubject("New feedback from " + employee.getName() + "" + employee.getSurname());
+        mailMessage.setText("plain text of email");
+
+        // Send mail
+        mailSender.send(mailMessage);
+    }
+}
